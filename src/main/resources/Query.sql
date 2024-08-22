591d5b30443dab478b4a33eb417cda9ac874fc85
Ruwan <ruwanliyanagegalle1994@gmail.com>
2024-08-22 23:49:41 +0530

    Add queries

---
+++ src/main/resources/Query.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `salary` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+//write a join query to get the employee name and department name
+SELECT e.name, d.name FROM employee e JOIN department d ON e.id = d.id;
+
