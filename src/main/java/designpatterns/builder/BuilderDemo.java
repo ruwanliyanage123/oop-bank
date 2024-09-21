d6bb7adf75d7d8825d958ab652879d5deca740c5
Ruwan <ruwanliyanagegalle1994@gmail.com>
2024-09-21 17:52:00 +0530

    Modification

--- src/main/java/designpatterns/builder/BuilderDemo.java
+++ src/main/java/designpatterns/builder/BuilderDemo.java
@@ -4,9 +4,7 @@ public class BuilderDemo {
     public static void main(String[] args) {
         Person person = new Person.PersonBuilder("John Doe")
                 .age(30)
+                .address("123 Main St")
                 .build();
-        System.out.println(person);
-        Person personWithoutAge = new Person.PersonBuilder("Jane Doe").build();
-        System.out.println(personWithoutAge);
     }
 }
--- src/main/java/designpatterns/builder/Person.java
+++ src/main/java/designpatterns/builder/Person.java
@@ -3,26 +3,42 @@ package designpatterns.builder;
 class Person {
     private final String name;
     private final int age;
+    private final String address;
+
     private Person(PersonBuilder builder) {
         this.name = builder.name;
         this.age = builder.age;
+        this.address = builder.address;
     }
     public String getName() {
         return name;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
     public int getAge() {
         return age;
     }
+
     public static class PersonBuilder {
         private final String name;
-        private int age = 0;
+        private int age;
+        private String address;
         public PersonBuilder(String name) {
             this.name = name;
         }
+
         public PersonBuilder age(int age) {
             this.age = age;
             return this;
         }
+        public PersonBuilder address(String address) {
+            this.address = address;
+            return this;
+        }
+
         public Person build() {
             return new Person(this);
         }
